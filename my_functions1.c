fffe02fe12393404c97e6ab9f1165e8887c1a231
nanaoliviane <nanaoliviane@gmail.com>
2023-09-19 12:15:52 -0700

    function that produces output according to a format

---
+++ my_functions1.c
@@ -0,0 +1,171 @@
+#include "main.h"
+
+/**
+ * print_strrev - function that prints a str in reverse
+ * @args: type struct va_arg where is allocated printf arguments
+ *
+ * Return: the string
+ */
+int print_strrev(va_list args)
+{
+
+	char *s = va_arg(args, char*);
+	int i;
+	int j = 0;
+
+	if (s == NULL)
+		s = "(null)";
+	while (s[j] != '\0')
+		j++;
+	for (i = j - 1; i >= 0; i--)
+		char_put(s[i]);
+	return (j);
+}
+
+/**
+ * printf_rot13 - printf str to ROT13 place into buffer
+ * @args: type struct va_arg where is allocated printf arguments
+ * Return: counter
+ *
+ */
+int printf_rot13(va_list args)
+{
+	int i, j, counter = 0;
+	int k = 0;
+	char *s = va_arg(args, char*);
+	char alpha[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+	char beta[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
+
+	if (s == NULL)
+		s = "(null)";
+	for (i = 0; s[i]; i++)
+	{
+		k = 0;
+		for (j = 0; alpha[j] && !k; j++)
+		{
+			if (s[i] == alpha[j])
+			{
+				char_put(beta[j]);
+				counter++;
+				k = 1;
+			}
+		}
+		if (!k)
+	{
+			char_put(s[i]);
+			counter++;
+		}
+	}
+	return (counter);
+}
+
+/**
+ * print_binary - prints a binary number.
+ * @val: arguments.
+ * Return: 1.
+ */
+int print_binary(va_list val)
+{
+	int flag = 0;
+	int cont = 0;
+	int i, a = 1, b;
+	unsigned int num = va_arg(val, unsigned int);
+	unsigned int p;
+
+	for (i = 0; i < 32; i++)
+	{
+		p = ((a << (31 - i)) & num);
+		if (p >> (31 - i))
+			flag = 1;
+		if (flag)
+		{
+			b = p >> (31 - i);
+			char_put(b + 48);
+			cont++;
+		}
+	}
+	if (cont == 0)
+	{
+		cont++;
+		char_put('0');
+	}
+	return (cont);
+}
+
+/**
+ * print_unsigned - prints integer
+ * @args: argument to print
+ * Return: number of characters printed
+ */
+int print_unsigned(va_list args)
+{
+	unsigned int n = va_arg(args, unsigned int);
+	int num, last = n % 10, digit, exp = 1;
+	int  i = 1;
+
+	n = n / 10;
+	num = n;
+
+	if (last < 0)
+	{
+		char_put('-');
+		num = -num;
+		n = -n;
+		last = -last;
+		i++;
+	}
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			char_put(digit + '0');
+			num = num - (digit * exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+	char_put(last + '0');
+
+	return (i);
+}
+
+/**
+ * print_octal - prints an octal number.
+ * @val: arguments.
+ * Return: counter.
+ */
+int print_octal(va_list val)
+{
+	int i;
+	int *array;
+	int counter = 0;
+	unsigned int num = va_arg(val, unsigned int);
+	unsigned int temp = num;
+
+	while (num / 8 != 0)
+	{
+		num /= 8;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(int));
+
+	for (i = 0; i < counter; i++)
+	{
+		array[i] = temp % 8;
+		temp /= 8;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		char_put(array[i] + '0');
+	}
+	free(array);
+	return (counter);
+}
---
+++ my_functions2.c
@@ -0,0 +1,98 @@
+#include "main.h"
+
+/**
+ * print_exclusive_string - print exclusuives string.
+ * @val: argument.
+ * Return: the length of the string.
+ */
+
+int print_exclusive_string(va_list val)
+{
+	char *s;
+	int i, len = 0;
+	int cast;
+
+	s = va_arg(val, char *);
+	if (s == NULL)
+		s = "(null)";
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] < 32 || s[i] >= 127)
+		{
+			char_put('\\');
+			char_put('x');
+			len = len + 2;
+			cast = s[i];
+			if (cast < 16)
+			{
+				char_put('0');
+				len++;
+			}
+			len = len + print_HEX_aux(cast);
+		}
+		else
+		{
+			char_put(s[i]);
+			len++;
+		}
+	}
+	return (len);
+}
+
+/**
+ * print_pointer - prints an hexgecimal number.
+ * @val: arguments.
+ * Return: counter.
+ */
+int print_pointer(va_list val)
+{
+	void *p;
+	char *s = "(nil)";
+	long int a;
+	int b;
+	int i;
+
+	p = va_arg(val, void*);
+	if (p == NULL)
+	{
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			char_put(s[i]);
+		}
+		return (i);
+	}
+
+	a = (unsigned long int)p;
+	char_put('0');
+	char_put('x');
+	b = print_hex_aux(a);
+	return (b + 2);
+}
+
+/**
+ * _strlen - lenght of a string.
+ * @s: Type char pointer
+ * Return: c.
+ */
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+		;
+	return (c);
+
+}
+/**
+ * _strlenc - Strlen
+ * @s: Type char pointer
+ * Return: c
+ */
+int _strlenc(const char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+		;
+	return (c);
+}
